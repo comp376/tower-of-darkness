50631f203eef327265b0b3d0120915b5deae07e2
layne91 <layne.brendan@gmail.com>
2013-11-24 15:30:01 -0500

    Added logic to breakable tiles

---
+++ tower-of-darkness-xna/tower-of-darkness-xna/tower-of-darkness-xna/Breakable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace tower_of_darkness_xna {
+    class Breakable {
+
+        public Rectangle bRect;
+        private float breakTimer = 0;
+        private float breakInterval = 500;
+        public bool isTouched = false;
+        public bool isBroken = false;
+        public int i;
+        public int j;
+
+        public Breakable(Rectangle bRect, int i, int j) {
+            this.bRect = bRect;
+            this.i = i;
+            this.j = j;
+        }
+
+        public void Update(GameTime gameTime) {
+            if (!isBroken && isTouched) {
+                breakTimer += gameTime.ElapsedGameTime.Milliseconds;
+                if (breakTimer >= breakInterval) {
+                    isBroken = true;
+                }
+            }
+        }
+
+    }
+}
